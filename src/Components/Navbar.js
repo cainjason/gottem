bab28dffce7b86d25a079fa509a1f0d8fe3d0cbf
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-01-01 18:09:24 -0500

    updated navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 function Navbar() {
   return (
     <ContainerNavbar>
-      <h2>Navbar</h2>
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/'>About page</a></li>
+        <li><a href='/'>Contact Page</a></li>
+      </ul>
     </ContainerNavbar>
   )
 }
@@ -13,6 +17,7 @@ export default Navbar;
  
 const ContainerNavbar = styled.div`
     background-color: greenyellow;
+    color: gray;
     width: 240px;
     height: 75vh;
 `; 
